1d999f11ee635afcc6c64cc0ee9ea462d829b3d0
maryam <maryam.rizwan10@icloud.com>
2024-08-12 03:31:17 +0500

    add ludo design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,185 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="left-box">
+
+        <div className="user-info">
+          <div className="dice-box"></div>
+          <div className="user-box">
+            <div className="user-dp-box">
+              <img className="user-dp" src='https://static.vecteezy.com/system/resources/previews/015/129/999/original/game-user-icon-png.png' />
+            </div>
+            <div className="userName">userName</div>
+          </div>
+        </div>
+
+        <div className="user-info">
+          <div className="dice-box"></div>
+          <div className="user-box">
+            <div className="user-dp-box">
+              <img className="user-dp" src='https://static.vecteezy.com/system/resources/previews/015/129/999/original/game-user-icon-png.png' />
+            </div>
+            <div className="userName">userName</div>
+          </div>
+        </div>
+       
+      </div>
+     <div className="ludo-box">
+    <div className="upper-container">
+      <div className="red-box">
+        <div>
+        <div className="red-token"></div>
+        <div className="red-token"></div>
+        </div>
+        <div>
+        <div className="red-token"></div>
+        <div className="red-token"></div>
+        </div>
+      </div>
+      <div className="red-green-middle-area">
+        <div className="zone"> <div className="inner-token"></div></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="green-fix-zone"></div>
+        <div className="green-fix-zone"></div>
+        <div className="red-fix-zone"></div>
+        <div className="green-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="green-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="green-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="green-fix-zone"></div>
+        <div className="zone"></div>
+      </div>
+      <div className="green-box">
+      <div>
+        <div className="green-token"></div>
+        <div className="green-token"></div>
+        </div>
+        <div>
+        <div className="green-token"></div>
+        <div className="green-token"></div>
+        </div>
+      </div>
+    </div>
+
+    <div className="middle-container">
+      <div className="red-yellow-middle-container">
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="red-fix-zone"></div>
+      <div className="red-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="red-fix-zone"></div>
+      <div className="yellow-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="red-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="red-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="red-fix-zone"></div>
+      <div className="zone"></div>
+      </div>
+
+      <div className="middle-box-middle-container"></div>
+
+      <div className="green-blue-middle-container">
+      <div className="zone"></div>
+      <div className="blue-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="blue-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="blue-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="green-fix-zone"></div>
+      <div className="blue-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="blue-fix-zone"></div>
+      <div className="blue-fix-zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      <div className="zone"></div>
+      </div>
+    </div>
+
+    <div className="lower-container">
+    <div className="yellow-box">
+        <div>
+        <div className="yellow-token"></div>
+        <div className="yellow-token"></div>
+        </div>
+        <div>
+        <div className="yellow-token"></div>
+        <div className="yellow-token"></div>
+        </div>
+      </div>
+      <div className="yellow-blue-middle-area">
+        <div className="zone"></div>
+        <div className="yellow-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="yellow-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="yellow-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="yellow-fix-zone"></div>
+        <div className="blue-fix-zone"></div>
+        <div className="yellow-fix-zone"></div>
+        <div className="yellow-fix-zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+        <div className="zone"></div>
+      </div>
+      <div className="blue-box">
+      <div>
+        <div className="blue-token"></div>
+        <div className="blue-token"></div>
+        </div>
+        <div>
+        <div className="blue-token"></div>
+        <div className="blue-token"></div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div className="right-box">
+    <div className="user-info">
+          <div className="user-box">
+            <div className="user-dp-box">
+              <img className="user-dp" src='https://static.vecteezy.com/system/resources/previews/015/129/999/original/game-user-icon-png.png' />
+            </div>
+            <div className="userName">userName</div>
+          </div>
+          <div className="dice-box"></div>
+        </div>
+
+        <div className="user-info">
+          <div className="user-box">
+            <div className="user-dp-box">
+              <img className="user-dp" src='https://static.vecteezy.com/system/resources/previews/015/129/999/original/game-user-icon-png.png' />
+            </div>
+            <div className="userName">userName</div>
+          </div>
+          <div className="dice-box"></div>
+        </div>
+    </div>
     </div>
   );
 }
